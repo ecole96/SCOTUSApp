3193df97172e4aa7284ba10cff0defbd2bda5f05
Evan Cole <ecole96@outlook.com>
2021-07-17 11:54:56 -0400

    accounting for the two different WSJ login pages at random intervals

--- articleCollector/SeleniumInstance.py
+++ articleCollector/SeleniumInstance.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
 import os
 
 # class for handling web scraping via Selenium
@@ -19,6 +20,8 @@ class SeleniumInstance:
         try:
             options = Options()
             options.headless = True
+            options.add_argument('--disable-blink-features=AutomationControlled')
+            options.add_experimental_option("excludeSwitches", ["enable-automation"])
             self.driver = webdriver.Chrome(options=options, executable_path='/usr/bin/chromedriver')
             self.driver.set_window_size(1200, 600) # even though we're headless this helps us avoid issues for some reason
         except Exception as e:
@@ -43,11 +46,30 @@ class SeleniumInstance:
                 login_url = "https://accounts.wsj.com/login"
                 self.driver.get(login_url)
                 wait = WebDriverWait(self.driver,30)
-                element_present = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR,"button.basic-login-submit"))) # wait until login button is visible to begin typing credentials
-                self.driver.find_element_by_name("username").send_keys(os.environ['WSJ_EMAIL']) # send credentials
-                self.driver.find_element_by_name("password").send_keys(os.environ['WSJ_PASSWORD'])
-                self.driver.find_element_by_css_selector("button.basic-login-submit").click() # login
-                element_present = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"#customer-nav-full-name"))) # wait until username appears on WSJ homepage after login to confirm login status
+                userNameReady = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"input.username"))) # wait until username can be typed in
+                # as of July 2020, WSJ seems to be randomly showing two different login screens
+                # one is basic: enter username, then password, then hit "Sign In" button
+                # the second requires an extra step: enter username, hit "Continue" button, enter password in box that appears, then "Sign In"
+                # whichever button ("Sign In" vs. "Continue") does not have style tag attached determines which process is used
+                try: 
+                    # if no exception triggers at this line, then basic login process used (this block is basically an if/else)
+                    BasicLoginAtStart = WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.CSS_SELECTOR,"button.basic-login-submit")))
+                    userNameReady.send_keys(os.environ['WSJ_EMAIL']) # type in username
+                    containerID = "basic-login"
+                except Exception: # Continue button not hidden (or doesn't exist, which will likely means an error will occur, probably due too site reformatting) - use alternate process
+                    print("Alternate Login")
+                    userNameReady.send_keys(os.environ['WSJ_EMAIL'])
+                    ContinueReady = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,"button.continue-submit")))
+                    ContinueReady.click()
+                    containerID = "password-login"
+                passwordSelector = 'div#%s input[name="password"]' % (containerID,)
+                passwordReadyToEnter = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR,passwordSelector)))
+                passwordReadyToEnter.click()
+                passwordReadyToEnter.send_keys(os.environ['WSJ_PASSWORD'])
+                loginSelector = 'div #%s button.basic-login-submit' % (containerID,)
+                loginReady = WebDriverWait(self.driver,5).until(EC.element_to_be_clickable((By.CSS_SELECTOR,loginSelector)))
+                loginReady.click()
+                redirectedToHomePage = wait.until(EC.presence_of_element_located((By.CSS_SELECTOR,"#customer-nav-full-name"))) # wait until username appears on WSJ homepage after login to confirm login status
                 self.loginStates['wsj'] = (True,True)
             except Exception as e:
                 print("WSJ Login Error: ",e)
